a3b273ee68621cea995b9af1092738eca5448f88
서동균 <seodong-gyun@seodong-gyun-ui-MacBookAir.local>
2023-06-21 01:21:58 +0900

    1

--- m02/ex03/Point.cpp
+++ m02/ex03/Point.cpp
@@ -7,9 +7,15 @@ Point::Point(const float xValue, const float yValue) : _x(xValue), _y(yValue) {}
 Point::Point(const Point& other) : _x(other._x), _y(other._y) {};
 
 Point Point::operator=(const Point& otehr) {
+	std::cout << "\033[1;31m[Warning] This is undefined behavior\033[0;39m" << std::endl;
 	if (this != &otehr) {
-		_x = otehr._x;
-		_y = otehr._y;
+		const Fixed *ptrX = &(_x);
+		Fixed *nonConstPtrX = const_cast<Fixed *>(ptrX);
+		*nonConstPtrX = otehr._x;
+
+		const Fixed *ptrY = &(_y);
+		Fixed *nonConstPtrY = const_cast<Fixed *>(ptrY);
+		*nonConstPtrY = otehr._y;
 	}
 	return (*this);
 }
