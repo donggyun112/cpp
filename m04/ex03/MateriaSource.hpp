1ca5dfe1fdb7c145b2e49e44c4874e94923840b2
서동균 <seodong-gyun@seodong-gyun-ui-MacBookAir.local>
2023-06-30 18:41:08 +0900

    1

---
+++ m04/ex03/MateriaSource.hpp
@@ -0,0 +1,15 @@
+#ifndef MATERIASOURCE_HPP
+# define MATERIASOURCE
+
+#include "IMateriaSource.hpp"
+#include <iostream>
+
+class MateriaSource : public IMateriaSource
+{
+	private:
+		std::string _type;
+	public:
+		
+};
+
+#endif
